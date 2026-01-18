7ca9be0f082efca883fd73730c99e3bf733934af
Pablo-Alemida345 <pabloalmeidadasilva@eaportal.org>
2026-01-18 19:41:20 -0300

    Adicionado modelo 3d no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import { Header } from "./components/Header"
+import { useEffect } from "react";
+import Header from "./components/Header"
+import Hero from "./components/Hero";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 export default function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration:1500,
+      once: true,
+    })
+  })
+  
+
   return(
     <main>
       {/* Gradient image */}
@@ -10,6 +24,7 @@ export default function App() {
       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
 
       <Header/>
+      <Hero/>
     </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,18 +17,18 @@ export const Header = () => {
   return (
     <header className="flex justify-between items-center py-4 px-4 lg:px-20">
 
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-light m-0">
+        <h1 data-aos='fade-down' data-aos-easing='linear' data-os-duration='1500' className="text-3xl md:text-4xl lg:text-5xl font-light m-0">
             P.CODE
         </h1>
 
         {/* Desktop Navigation */}
 
         <nav className="hidden md:flex items-center gap-12">
-            <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+            <a data-aos='fade-down' data-aos-easing='linear' data-os-duration='1000' className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
                 EMPRESA
             </a>
             
-            <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+            <a data-aos='fade-down' data-aos-easing='linear' data-os-duration='1500' className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
                 FEATURES
             </a>
 
